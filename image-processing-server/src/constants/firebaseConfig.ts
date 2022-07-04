a654a2fdd9e4f72324c692c85aa02db0d0d5a4fb
michael ameteku <31578930+ameteku@users.noreply.github.com>
2022-07-04 00:29:18 -0700

    Delete firebaseConfig.ts

--- image-processing-server/src/constants/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyBcFcWYgXE7X01GW_VQBoAXtUv3a4AH_Ak",
-    authDomain: "lapnitor.firebaseapp.com",
-    projectId: "lapnitor",
-    storageBucket: "lapnitor.appspot.com",
-    messagingSenderId: "109908020922",
-    appId: "1:109908020922:web:11f75349db890b5c9bfc83",
-    measurementId: "G-365BRPNKCX"
-  };
